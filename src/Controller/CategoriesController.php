a2e294d7a7b1ba56e7fc4463081109be970b5262
aynarakotovao <ayna.manao@gmail.com>
2023-07-17 19:01:25 +0300

    pagination sans bundle

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -3,7 +3,9 @@
     namespace App\Controller;
 
     use App\Entity\Categories;
+    use App\Repository\ProductsRepository;
     use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+    use Symfony\Component\HttpFoundation\Request;
     use Symfony\Component\HttpFoundation\Response;
     use Symfony\Component\Routing\Annotation\Route;
     
@@ -19,10 +21,18 @@
         }
 
         #[Route("/{slug}", name : "list")]
-        public function details(Categories $category): Response
+        public function details(Categories $category, 
+            ProductsRepository $productsRepository,
+            Request $request
+            ): Response
         {
+            // On va chercher le numero de la page dans l'URL
+            $page = $request->query->getInt('page',1);
+
             // On va chercher tous les produits de la catégorie
-            $products = $category->getProducts();
+            // $products = $category->getProducts();
+            $products = $productsRepository->findProductsPaginated($page,$category->getSlug(), 2);
+
             return $this->render("categories/list.html.twig", compact(
                     'category','products'
                 )
